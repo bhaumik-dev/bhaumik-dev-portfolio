b451eccceebdb6498d9d0a17f8ab0d2e7aad60cf
Bhaumik Raja <webdevbhaumik@gmail.com>
2024-10-09 17:55:30 -0400

    Completed marquee tape without animation

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,57 @@
+import StarIcon from "@/assets/icons/star.svg";
+
+const words = [
+  "Scalabale",
+  "Responsive",
+  "User-Friendly",
+  "Search Optimized",
+  "Accessible",
+  "Secure",
+];
+
+const technologies = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "Tailwind CSS",
+  "React",
+  "Next",
+  "Webflow",
+  "WordPress",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div className="py-12 lg:py-24 overflow-x-clip">
+      {/* Tape 1 */}
+      <div className="bg-gradient-to-r from-violet-500 to-purple-600 -rotate-2 -mx-1">
+        <div className="flex [mask-image:linear-gradient(to_right, transparent, black_10%, black_90%, transparent)]">
+          <div className="flex flex-none gap-4 py-4 ">
+            {words.map((word) => (
+              <div key={word} className="inline-flex gap-8 items-center">
+                <span className="uppercase font-semibold text-md">{word}</span>
+                <StarIcon className="size-6 -rotate-8" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* Tape 2 */}
+      <div className="bg-gradient-to-r from-violet-500 to-purple-600 -rotate-2 -mx-1 mt-2">
+        <div className="flex [mask-image:linear-gradient(to_right, transparent, black_10%, black_90%, transparent)]">
+          <div className="flex flex-none gap-4 py-4 ">
+            {technologies.map((technology) => (
+              <div key={technology} className="inline-flex gap-8 items-center">
+                <span className="uppercase font-semibold text-md">
+                  {technology}
+                </span>
+                <StarIcon className="size-6 -rotate-8" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
