57ac234ad2e77ef773f7df6d4778f6e61b657ffa
Bhaumik Raja <webdevbhaumik@gmail.com>
2024-10-29 20:21:12 -0400

    Updated Portfolio v1 with Favicon and better page load animation.

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -7,25 +7,41 @@ const Loader: React.FC = () => {
   const [progress, setProgress] = useState(0);
 
   useEffect(() => {
-    // GSAP Tween for a simple counter from 0 to 100
     gsap.to(
       {},
       {
-        duration: 3, // Duration of the loading animation
+        duration: 3,
         onUpdate: function () {
-          setProgress(Math.floor(this.progress() * 100)); // Update progress
+          setProgress(Math.floor(this.progress() * 100));
         },
         onComplete: () => {
-          // Optional: Add code to hide loader and show content after loading completes
           document.getElementById("loader")?.classList.add(styles.hide);
         },
       }
     );
   }, []);
 
+  // Array for repeated text
+  const marqueeText = Array(20).fill("Frontend Development");
+  const marqueeText2 = Array(20).fill("No-Code Development");
+
   return (
     <div id="loader" className={styles.loaderContainer}>
+      <div className={styles.marquee}>
+        <div className={styles.marqueeText}>
+          {marqueeText.map((text, index) => (
+            <p key={index}>{text}</p>
+          ))}
+        </div>
+      </div>
       <div className={styles.loaderText}>{progress}%</div>
+      <div className={styles.marquee2}>
+        <div className={styles.marqueeText2}>
+          {marqueeText2.map((text, index) => (
+            <p key={index}>{text}</p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
